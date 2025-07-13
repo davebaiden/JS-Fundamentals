a980c6e1514109c84512ec776f106d053cd7fddc
David Baiden Amissah <davidbamissah@gmail.com>
2025-07-13 05:17:45 +0000

    Add 10-factorial.js

---
+++ 10-factorial.js
@@ -0,0 +1,8 @@
+function factorial(n) {
+  if (n <= 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+const input = Number(process.argv[2]);
+console.log(factorial(isNaN(input) ? 0 : input));
